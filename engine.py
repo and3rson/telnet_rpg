294f109e08b7e2b20227aebcfdc2e981bc38acdb
and3rson <anderson.dunai@gmail.com>
2015-12-07 11:41:09 +0200

    WIP

--- engine.py
+++ engine.py
@@ -1,9 +1,15 @@
-from protocol import Commands, Utils
+from protocol import Commands, Utils, ANSI
+import traceback
 
 clients = []
 
 
 class ClientHandler(object):
+    class State(object):
+        IDLE = 0
+        EXPECT_COMMAND = 1
+        SUBNEGOTIATION = 3
+
     @classmethod
     def create(cls, socket, address):
         client = ClientHandler(socket, address)
@@ -14,6 +20,10 @@ class ClientHandler(object):
         self.address = address
         self.alive = True
         self.stack = []
+        self.state = ClientHandler.State.IDLE
+
+        self.cmd = None
+        self.args = None
 
         print 'New connection from {}:{}'.format(*self.address)
         clients.append(self)
@@ -23,7 +33,9 @@ class ClientHandler(object):
     def prepare(self):
         self.socket.send(Commands.IAC + Commands.WILL + Commands.ECHO)
         self.socket.send(Commands.IAC + Commands.WILL + Commands.SUPPRESS_GO_AHEAD)
-        self.socket.send(Commands.IAC + Commands.WONT + Commands.EXOPL)  # TODO: Unsure about this one
+        # self.socket.send(Commands.IAC + Commands.WONT + Commands.EXOPL)  # TODO: Unsure about this one
+
+        self.socket.send(Commands.IAC + Commands.DO + Commands.NAWS)
 
         self.socket.send(Commands.ESC + '[2J')
         self.socket.send(Commands.ESC + '[0;0H')
@@ -32,38 +44,119 @@ class ClientHandler(object):
 
     def start(self):
         while self.alive:
-            data = self.socket.recv(1)
+            data = self.socket.recv(1024)
             if not data:
                 print 'Connection {}:{} dropped'.format(*self.address)
                 clients.remove(self)
                 self.alive = False
             else:
                 print 'Got data:', repr(data)
-            self.stack.append(data)
+            self.stack.extend(data)
 
             self.process()
 
     def process(self):
-        if not len(self.stack):
-            return
+        while len(self.stack):
+            head = self.stack[0]
+            if head == Commands.IAC and len(self.stack) >= 2:
+                op = self.stack[1]
 
-        head = self.stack[0]
-        if head == Commands.IAC:
-            if len(self.stack) >= 3:
+                if len(self.stack) >= 3:
+                    iac = self.stack.pop(0)
+                    op = self.stack.pop(0)
+                    action = self.stack.pop(0)
+                    print 'Got command:', Commands.decode_cmd(iac, op, action)
+
+                    if op == Commands.SB:
+                        data = []
+                        while self.stack[0:2] != [Commands.IAC, Commands.SE]:
+                            b = self.stack.pop(0)
+                            data.append(b)
+                        self.stack.pop(0)
+                        self.stack.pop(0)
+
+                        print 'Subnegotiation data:', [ord(x) for x in data]
+
+                        self.handle_command(op, action, data)
+
+                    continue
+                # elif op == Commands.SB and (Commands.IAC + Commands.SE in ''.join(self.stack)):
+                #     iac = self.stack.pop(0)
+                #     op = self.stack.pop(0)
+                #     action = self.stack.pop(0)
+                #
+                #
+                #     continue
+                else:
+                    return
+
+            elif Utils.is_ascii(head):
                 self.stack.pop(0)
-                print 'Got command:', Commands.decode_cmd(self.stack.pop(0) + self.stack.pop(0))
-            return
+                self.socket.send(head)
 
-        elif Utils.is_ascii(head):
-            self.stack.pop(0)
-            self.socket.send(head)
-        elif head == '\x0D':
-            self.stack.pop(0)
-            self.socket.send('\x0D\x0A')
+                continue
 
-        else:
-            print 'Unexpected byte - {}'.format(repr(head))
-            self.stack.pop(0)
+            elif head == '\x0D':
+                self.stack.pop(0)
+                self.socket.send('\x0D\x0A')
 
-            # ANSI escape codes: http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
-            # self.socket.send(Commands.ESC + '[1B')
+            elif head == '\t':
+                self.stack.pop(0)
+                self.socket.send('<TAB>')
+
+                continue
+            elif head == Commands.ESC:
+                # if len(self.stack) >= 2:
+                #     self.socket.send(self.stack)
+                # else:
+
+                self.stack.pop(0)
+
+                # TODO: Implement escape char parsing
+                code_len, value = ANSI.inspect(self.stack)
+                self.socket.send('<{}>'.format(value))
+
+                for i in xrange(code_len):
+                    self.stack.pop(0)
+
+                    # self.socket.send('<ESCAPE>')
+
+                continue
+
+            elif head == Commands.CTRL_BREAK:
+                self.stack.pop(0)
+                self.socket.send('<CTRL+C>')
+
+                continue
+
+            elif head == '\x7F':
+                self.stack.pop(0)
+                self.socket.send('<BACKSPACE>')
+
+                continue
+
+            elif head == Commands.NULL:
+                self.stack.pop(0)
+
+            else:
+                print 'Unexpected byte - {}'.format(repr(head))
+                b = self.stack.pop(0)
+
+                self.socket.send('<0x{}>'.format('%02X' % ord(b)))
+
+                return
+
+                # ANSI escape codes: http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
+                # self.socket.send(Commands.ESC + '[1B')
+
+    def handle_command(self, op, action, data):
+        if op == Commands.SB and action == Commands.NAWS:
+            try:
+                print '* Client screen size: {}x{}'.format(
+                    Utils.decode_word(data[0:2]),
+                    Utils.decode_word(data[2:4]),
+                )
+            except:
+                exc = traceback.format_exc()
+                print exc
+                self.socket.send(exc)
--- protocol.py
+++ protocol.py
@@ -1,4 +1,8 @@
+import struct
+
+
 class Commands:
+    NULL = '\x00'
     IAC = '\xFF'
 
     WILL = '\xFB'
@@ -8,32 +12,107 @@ class Commands:
 
     ECHO = '\x01'
     SUPPRESS_GO_AHEAD = '\x03'
-    EXOPL = '\xFF'
+    NAWS = '\x1F'
+
+    SB = '\xFA'
+    SE = '\xF0'
 
     ESC = '\x1B'
 
-    CMD_TYPE = {
+    CTRL_BREAK = '\x03'
+
+    CMD_DICT = {
+        IAC: 'IAC',
         WILL: 'WILL',
         WONT: 'WONT',
         DO: 'DO',
-        DONT: 'DONT'
-    }
-
-    CMD_NAME = {
+        DONT: 'DONT',
+        SB: 'SB',
+        SE: 'SE',
         ECHO: 'ECHO',
         SUPPRESS_GO_AHEAD: 'SUPPRESS-GO-AHEAD',
-        EXOPL: 'EXOPL'
+        NAWS: 'NAWS',
+        CTRL_BREAK: 'CTRL_BREAK',
+        NULL: 'NULL',
     }
 
     @classmethod
-    def decode_cmd(cls, cmd):
-        return u'{} {}'.format(
-            cls.CMD_TYPE.get(cmd[0], '???'),
-            cls.CMD_NAME.get(cmd[1], '???')
-        )
+    def decode_cmd(cls, *args):
+        return ' '.join(cls.CMD_DICT.get(code) for code in args)
 
 
 class Utils:
     @classmethod
     def is_ascii(cls, char):
         return 32 <= ord(char) <= 126
+
+    @classmethod
+    def decode_word(cls, chars):
+        return struct.unpack('>H', ''.join(chars))[0]
+
+
+class ANSI:
+    class Ranges:
+        C0 = [(0, 0x1F)]
+        SPACE = [(0x20, 0x20), (0xA0, 0xA0)]
+        INTERMEDIATE = [(0x20, 0x2F)]
+        PARAMETERS = [(0x30, 0x3F)]
+        UPPERCASE = [(0x40, 0x5F)]
+        LOWERCASE = [(0x60, 0x7E)]
+        ALPHABETIC = [(0x40, 0x7E)]
+        DELETE = [(0x7F, 0x7F)]
+        C1 = [(0x80, 0x9F)]
+        G1 = [(0xA1, 0xFE)]
+        SPECIAL = [(0xA0, 0xA0), (0xFF, 0xFF)]
+
+    KEYS = {
+        '[A': 'UP',
+        '[B': 'DOWN',
+        '[C': 'RIGHT',
+        '[D': 'LEFT',
+        'OP': 'F1',
+        'OQ': 'F2',
+        'OR': 'F3',
+        'OS': 'F4',
+
+        'OH': 'HOME',
+        'OF': 'END',
+
+        '[1~': 'FIND',
+        '[2~': 'INSERT',
+        '[3~': 'DELETE',
+        '[4~': 'SELECT',
+        '[5~': 'PG_UP',
+        '[6~': 'PG_DOWN',
+        '[E~': 'KP_5',
+    }
+
+    @classmethod
+    def is_in(cls, char, check_range):
+        for _min, _max in check_range:
+            if _min <= ord(char) <= _max:
+                return True
+        return False
+
+    @classmethod
+    def inspect(cls, data):
+        data = ''.join(data)
+        for code, value in ANSI.KEYS.items():
+            if data.startswith(code):
+                return len(code), value
+        return 0, '?'
+
+        # if data[0] == '[' and data[1] in 'ABCD':
+        #     return dict(
+        #         A=ANSI.Keys.UP,
+        #         B=ANSI.Keys.DOWN,
+        #         C=ANSI.Keys.RIGHT,
+        #         D=ANSI.Keys.LEFT,
+        #     ).get(data[1])
+        # elif data[0] == 'O' and data[1] in 'OPQR':
+        #     return dict(
+        #         O=ANSI.Keys.F1,
+        #         P=ANSI.Keys.F2,
+        #         Q=ANSI.Keys.F3,
+        #         R=ANSI.Keys.F4,
+        #     ).get(data[1])
